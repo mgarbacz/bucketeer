7034c1a7f00b09c2beddbc526342a389aaab895b
mgarbacz <mich.garbacz@gmail.com>
2013-06-24 01:26:20 -0500

    Initial upload code.

---
+++ bucketeer/__init__.py
@@ -0,0 +1,9 @@
+"""
+    s3-committer is a python module that commits your files to Amazon's
+    S3 storage.
+"""
+
+__version__ = '0.0.1'
+__project__ = 's3-committer'
+__author__ = 'Michal Garbacz <mich.garbacz+dev@gmail.com>'
+__license__ = 'MIT'
---
+++ commit.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+from bucketeer import main
+
+main.commit_to_s3('test.michgarbacz', 'test')
+
+print 'Task done.'
