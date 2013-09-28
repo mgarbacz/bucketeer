f6c462db3a1180798fe75fac4b9fbb09c4969ccd
mgarbacz <mich.garbacz@gmail.com>
2013-09-28 18:39:48 -0500

    Added test.py and moved testing files
    Added a test.py file to begin tests for Bucketeer. Moved files used for
    testing uploading into `test.bucket` folder.

---
+++ test/test.py
@@ -0,0 +1,17 @@
+import unittest
+
+class BuckeeterTest(unittest.TestCase):
+
+  def setUp(self):
+    # 1 bucket with 1 file
+    return
+    
+  def tearDown(self):
+    # Remove all test-created buckets and files
+    return
+
+  def testMain(self):
+    self.assertTrue(main)
+
+if __name__ == '__main__':
+  unittest.main()
