88f9b578ea9476ee1c328a51547b6418a51986b9
mgarbacz <mich.garbacz@gmail.com>
2013-10-02 12:14:02 -0500

    Add more info to setup.py
    Added author, author_email, description, and install_requires to
    setup.py.

--- setup.py
+++ setup.py
@@ -3,8 +3,14 @@ from distutils.core import setup
 setup(
   name='Bucketeer',
   version='0.1.0',
-  packages=['buckeeter',],
+  author='Michal Garbacz',
+  author_email='mich.garbacz+dev@gmail.com',
+  packages=['bucketeer',],
   license='MIT License',
+  description='Commits files to Amazon\'s S3 storage',
   long_description=open('README.md').read(),
+  install_requires=[
+    "boto == 2.6.0",
+  ],
 )
 
