9a57f2493a8e7561a053077c793cdd998c9a28c9
mgarbacz <mich.garbacz@gmail.com>
2013-10-28 22:37:47 -0500

    Add setUp and tearDown of a test bucket

--- bucketeer/test/test_commit.py
+++ bucketeer/test/test_commit.py
@@ -1,14 +1,20 @@
 import unittest
+import boto
 from bucketeer import commit
 
 class BuckeeterTest(unittest.TestCase):
 
   def setUp(self):
-    # 1 bucket with 1 file
+    # Create a bucket with one file
+    connection = boto.connect_s3()
+    bucket = connection.create_bucket('bucket.exists')
+
     return
-    
+
   def tearDown(self):
     # Remove all test-created buckets and files
+    connection = boto.connect_s3()
+    bucket = connection.delete_bucket('bucket.exists')
     return
 
   def testMain(self):
