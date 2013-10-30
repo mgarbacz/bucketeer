ed8423041abc80b778bf9ffed61e3dad246d72ff
mgarbacz <mich.garbacz@gmail.com>
2013-10-29 19:45:22 -0500

    Refactor bucket name to global variable

--- bucketeer/test/test_commit.py
+++ bucketeer/test/test_commit.py
@@ -4,17 +4,20 @@ from bucketeer import commit
 
 class BuckeeterTest(unittest.TestCase):
 
+  global existing_bucket
+  existing_bucket = 'bucket.exists'
+
   def setUp(self):
     # Create a bucket with one file
     connection = boto.connect_s3()
-    bucket = connection.create_bucket('bucket.exists')
+    bucket = connection.create_bucket(existing_bucket)
 
     return
 
   def tearDown(self):
     # Remove all test-created buckets and files
     connection = boto.connect_s3()
-    bucket = connection.delete_bucket('bucket.exists')
+    bucket = connection.delete_bucket(existing_bucket)
     return
 
   def testMain(self):
