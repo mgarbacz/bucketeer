c973068ada6fa5039a289719c852f06fe786c8fa
mgarbacz <mich.garbacz@gmail.com>
2013-10-30 23:31:00 -0500

    Add create test file dir to setUp and tearDown
    May need to revisit and identify best place to put such a directory.

--- bucketeer/test/test_commit.py
+++ bucketeer/test/test_commit.py
@@ -1,23 +1,30 @@
-import unittest
-import boto
+import unittest, boto, os
 from bucketeer import commit
 
 class BuckeeterTest(unittest.TestCase):
 
-  global existing_bucket
+  global existing_bucket, test_dir
   existing_bucket = 'bucket.exists'
+  test_dir = 'bucketeer_test_dir'
 
   def setUp(self):
-    # Create a bucket with one file
+    # Create a bucket to test on existing bucket
     connection = boto.connect_s3()
     bucket = connection.create_bucket(existing_bucket)
+
+    # Create directory to house test files
+    os.makedirs(test_dir)
 
     return
 
   def tearDown(self):
-    # Remove all test-created buckets and files
+    # Remove bucket created to test on existing bucket
     connection = boto.connect_s3()
     bucket = connection.delete_bucket(existing_bucket)
+
+    # Remove directory created to house test files
+    os.rmdir(test_dir)
+
     return
 
   def testMain(self):
