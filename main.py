51de315b5752e420a5d787932e078f349291a066
mgarbacz <mich.garbacz@gmail.com>
2013-11-22 01:09:43 -0600

    Add upload_from_config to main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 from bucketeer import uploader
 import sys
 
-uploader.upload(sys.argv[1], sys.argv[2])
+try:
+  with open('config.json'):
+    uploader.upload_from_config()
+except IOError:
+  print 'Configuration file not found. Using command line arguments.'
+  uploader.upload(sys.argv[1], sys.argv[2])
 
 print 'Task done.'
