78137badf769c9d14564af1cef54641b620d9d70
mgarbacz <mich.garbacz@gmail.com>
2013-11-24 00:12:42 -0600

    Update version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='Bucketeer',
-  version='1.0.0',
+  version='1.1.1',
   author='Michal Garbacz',
   author_email='mich.garbacz+dev@gmail.com',
   packages=['bucketeer',],
