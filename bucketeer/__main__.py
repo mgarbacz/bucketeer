6ac28e9901a565b69953fcc03525c2674c9a7ffa
mgarbacz <mich.garbacz@gmail.com>
2013-11-28 01:59:13 -0600

    Cleaning up __main__
    Use if __name__ as entry point and move config/command logic into here.

--- bucketeer/__main__.py
+++ bucketeer/__main__.py
@@ -2,11 +2,17 @@
 from bucketeer import uploader
 import sys
 
-try:
-  with open('config.json'):
-    uploader.upload_from_config()
-except IOError:
-  print 'Configuration file not found. Using command line arguments.'
-  uploader.upload(sys.argv[1], sys.argv[2])
+def main(bucket = false, directory = false):
+  if (not bucket and not directory):
+    try:
+      with open('config.json'):
+        uploader.upload_from_config()
+    except IOError:
+      print 'Configuration file not found. Using command line arguments.'
+      uploader.upload(bucket, directory)
 
 print 'Task done.'
+
+if __name__ == '__main__':
+  # Map command line arguments to function arguments
+  main(*sys.argv[1:])
