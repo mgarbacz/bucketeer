7a1aab5968ebec781e60e01647e82547008de5db
mgarbacz <mich.garbacz@gmail.com>
2013-11-28 19:41:38 -0600

    Reverse if logic, add KeyError exception handle

--- bucketeer/__main__.py
+++ bucketeer/__main__.py
@@ -3,15 +3,18 @@ from bucketeer import uploader
 import sys, json
 
 def main(bucket = False, directory = False):
-  if (not bucket and not directory):
+  if bucket and directory:
+    uploader.upload(bucket, directory)
+  else:
     try:
       config = json.loads(open('bucketeer.json').read())
       print 'Configuration file found.'
       uploader.upload(config['bucket'], config['directory'])
     except IOError:
       print 'Configuration file not found. Nothing to do...'
-  else:
-    uploader.upload(bucket, directory)
+    except KeyError as e:
+      print 'Configuration file incorrect.', e
+
   print 'Task done.'
 
 if __name__ == '__main__':
