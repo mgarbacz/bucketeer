407938ca0c190c0d75849a62ffa238feb4176d11
mgarbacz <mich.garbacz@gmail.com>
2013-11-30 20:43:32 -0600

    Move config checking into uploader.py

--- bucketeer/__main__.py
+++ bucketeer/__main__.py
@@ -3,18 +3,7 @@ import sys, json
 from bucketeer import uploader
 
 def main(bucket = False, directory = False):
-  if bucket and directory:
-    uploader.upload(bucket, directory)
-  else:
-    try:
-      config = json.loads(open('bucketeer.json').read())
-      print 'Configuration file found.'
-      uploader.upload(config['bucket'], config['directory'])
-    except IOError:
-      print 'Configuration file not found. Nothing to do...'
-    except KeyError as e:
-      print 'Configuration file incorrect. Missing:', e
-
+  uploader.upload(bucket, directory)
   print 'Task done.'
 
 if __name__ == '__main__':
--- bucketeer/uploader.py
+++ bucketeer/uploader.py
@@ -1,10 +1,23 @@
-import boto, os, hashlib
+import boto, os, hashlib, json
 
-# Upload modified files in src_folder to the s3 bucket named
-def upload(bucket_name, src_folder):
+# Upload modified files in src_directory to the given s3 bucket
+def upload(bucket_name, src_directory):
   success = False
 
-  print 'Attempting to upload %s to %s' % (src_folder, bucket_name)
+  if not (bucket_name and src_directory):
+    try:
+      config = json.loads(open('bucketeer.json').read())
+      print 'Configuration file found.'
+      bucket_name = config['bucket']
+      src_directory = config['directory']
+    except IOError:
+      print 'Configuration file not found. Nothing to do...'
+      return
+    except KeyError as e:
+      print 'Configuration file incorrect. Missing:', e
+      return
+
+  print 'Attempting to upload %s to %s' % (src_directory, bucket_name)
   try:
     # Requires S3 creds, which are set as environment variables
     connection = boto.connect_s3();
@@ -16,14 +29,14 @@ def upload(bucket_name, src_folder):
       bucket = connection.create_bucket(bucket_name)
       print 'Bucket %s created.' % bucket_name
 
-    delete_files(src_folder, bucket)
+    delete_files(src_directory, bucket)
 
     # Iterating over all files in the src folder
-    for directory, subdirectories, files in os.walk(src_folder):
+    for directory, subdirectories, files in os.walk(src_directory):
 
       # Upload each local file in files
       for filename in files:
-        upload_file(filename, directory, src_folder, bucket)
+        upload_file(filename, directory, src_directory, bucket)
 
     # If we got here with no exceptions, changes have been committed
     success = True
@@ -39,11 +52,11 @@ def upload(bucket_name, src_folder):
 
   return success
 
-def upload_file(filename, directory, src_folder, bucket):
+def upload_file(filename, directory, src_directory, bucket):
   # Set up file paths and get s3 file
   local_file_path = os.path.join(directory, filename)
   # For s3, we don't want the 'public' part of file path
-  s3_file_path = local_file_path[len(src_folder)+1:]
+  s3_file_path = local_file_path[len(src_directory)+1:]
   s3_file = bucket.get_key(s3_file_path)
 
   # If file exists: compare hashes, else: force unmatching hashes
@@ -67,11 +80,11 @@ def upload_file(filename, directory, src_folder, bucket):
   # Will print after update or if no update was required
   print '%s is up to date.' % filename
 
-def delete_files(src_folder, bucket):
+def delete_files(src_directory, bucket):
   # Delete each s3 file not present locally
   for s3_file in bucket.list():
     try:
-      with open(os.path.join(src_folder, s3_file.key)):
+      with open(os.path.join(src_directory, s3_file.key)):
         print '%s exists locally.' % s3_file.key
     except IOError:
       print '%s is being deleted from s3...' % s3_file.key
